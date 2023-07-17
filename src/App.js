e04930943368528d612f6c713e7ff152947213ab
kylemcnally10 <109179389+kylemcnally10@users.noreply.github.com>
2023-07-17 14:29:45 -0700

    one more try

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import About from './components/About';
 
 function App() {
   return (
-    <Router basename="/">
+    <Router>
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
