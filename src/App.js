74901741690b6e942bd6d780dbe7267165a18a4a
kylemcnally10 <109179389+kylemcnally10@users.noreply.github.com>
2023-07-17 13:44:10 -0700

    fixing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import About from './components/About';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/portfolio-site">
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
